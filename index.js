56724eb9f5202fdf6507dcb32b4a47f6f0ad67e7
Neill Robson <neill.robson@pendo.io>
2021-09-15 10:44:06 -0400

    Change lint rule to be vue-specific

--- index.js
+++ index.js
@@ -1,12 +1,87 @@
+const utils = require('eslint-plugin-vue/lib/utils');
+
 module.exports = {
     rules: {
-        "no-template-literals": {
-            create: context => {
-                return {
-                    TemplateLiteral(node) {
-                        context.report(node, 'Neill says: do not use template literals');
+        "no-expressions": {
+            create(context) {
+                // const options = context.options[0] || 'always'
+                const template =
+                    context.parserServices.getTemplateBodyTokenStore &&
+                    context.parserServices.getTemplateBodyTokenStore()
+
+                // ----------------------------------------------------------------------
+                // Public
+                // ----------------------------------------------------------------------
+
+                return utils.defineTemplateBodyVisitor(context, {
+                    /** @param {VExpressionContainer} node */
+                    'VExpressionContainer[expression!=null]'(node) {
+                        const openBrace = template.getFirstToken(node)
+                        const closeBrace = template.getLastToken(node)
+
+                        if (
+                            !openBrace ||
+                            !closeBrace ||
+                            openBrace.type !== 'VExpressionStart' ||
+                            closeBrace.type !== 'VExpressionEnd'
+                        ) {
+                            return
+                        }
+
+                        context.report({
+                            node: openBrace,
+                            message: "Neill says: he doesn't like these nodes.",
+                            fix: (fixer) => fixer.insertTextAfter(openBrace, 'Neill!')
+                        })
+
+                        // const firstToken = template.getTokenAfter(openBrace, {
+                        //     includeComments: true
+                        // })
+                        // const lastToken = template.getTokenBefore(closeBrace, {
+                        //     includeComments: true
+                        // })
+
+                        // if (options === 'always') {
+                        //     if (openBrace.range[1] === firstToken.range[0]) {
+                        //         context.report({
+                        //             node: openBrace,
+                        //             message: "Expected 1 space after '{{', but not found.",
+                        //             fix: (fixer) => fixer.insertTextAfter(openBrace, ' ')
+                        //         })
+                        //     }
+                        //     if (closeBrace.range[0] === lastToken.range[1]) {
+                        //         context.report({
+                        //             node: closeBrace,
+                        //             message: "Expected 1 space before '}}', but not found.",
+                        //             fix: (fixer) => fixer.insertTextBefore(closeBrace, ' ')
+                        //         })
+                        //     }
+                        // } else {
+                        //     if (openBrace.range[1] !== firstToken.range[0]) {
+                        //         context.report({
+                        //             loc: {
+                        //                 start: openBrace.loc.start,
+                        //                 end: firstToken.loc.start
+                        //             },
+                        //             message: "Expected no space after '{{', but found.",
+                        //             fix: (fixer) =>
+                        //                 fixer.removeRange([openBrace.range[1], firstToken.range[0]])
+                        //         })
+                        //     }
+                        //     if (closeBrace.range[0] !== lastToken.range[1]) {
+                        //         context.report({
+                        //             loc: {
+                        //                 start: lastToken.loc.end,
+                        //                 end: closeBrace.loc.end
+                        //             },
+                        //             message: "Expected no space before '}}', but found.",
+                        //             fix: (fixer) =>
+                        //                 fixer.removeRange([lastToken.range[1], closeBrace.range[0]])
+                        //         })
+                        //     }
+                        // }
                     }
-                };
+                })
             }
         }
     }
