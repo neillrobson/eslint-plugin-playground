727bfe5a66008a1a65e30a05bdd88849fb687433
Neill Robson <neill.robson@pendo.io>
2021-10-08 15:42:16 -0400

    Add explicit plugin reference to eslintrc
    Unless a plugin config specifies itself in its `plugins` array, we need
    to specify it ourselves in our configuration for the rules to be found.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
     root: true,
+    plugins: ['eslint-plugin'],
     extends: ['eslint:recommended', 'plugin:eslint-plugin/recommended', 'plugin:node/recommended', 'prettier'],
     env: {
         node: true
