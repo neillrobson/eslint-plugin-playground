d56b2888983db6b00eac5d2edf4ffc9d58841b29
Neill Robson <neill.robson@pendo.io>
2021-10-08 11:49:56 -0400

    Add "all" config to plugin exports
    Now, users of the plugin can simply add `plugin:playground/all` to their
    .eslintrc's extends array to get the benefit of all rules in the plugin.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const rawHighcharts = require('./rules/raw-highcharts');
 
 module.exports = {
+    configs: {
+        all: {
+            plugins: ['playground'],
+            rules: {
+                'playground/raw-highcharts': 'warn'
+            }
+        }
+    },
     rules: {
         'raw-highcharts': rawHighcharts
     }
